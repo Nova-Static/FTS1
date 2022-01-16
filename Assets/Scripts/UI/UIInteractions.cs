3de106fd1fd463ab89e1d9599479dfcf6d205482
Nova-Static <48789639+Nova-Static@users.noreply.github.com>
2022-01-16 10:56:35 +0100

    Revert "Merge branch 'main' of https://github.com/Nova-Static/FTS1"
    This reverts commit 63f9c3ec1d085688aada8d358dafe39a7d69d389, reversing
    changes made to 2aa6d61995b5ac700ed2b8a08ceb4361a2cb30e7.

--- Assets/Scripts/UI/UIInteractions.cs
+++ Assets/Scripts/UI/UIInteractions.cs
@@ -8,9 +8,6 @@ public class UIInteractions : MonoBehaviour
 {
     public GameObject AccountPanel;
     public Transform AccountBox;
-    public GameObject CalendarPanel;
-    public Transform CalendarBox;
-    
     public CanvasGroup Background;
     
     public Canvas BGCanvas;
@@ -39,33 +36,16 @@ public class UIInteractions : MonoBehaviour
         AccountBox.LeanMoveLocalX(0, 0.5f).setEaseOutExpo().delay = 0.2f;
     }
 
-    public void CalendarPress()
-    {
-        CalendarPanel.SetActive(true);
-        CalendarBox.localPosition = new Vector2(-Screen.width, 0);
-        CalendarBox.LeanMoveLocalX(0, 0.5f).setEaseOutExpo().delay = 0.2f;
-    }
-
     //back to main screen from account panel
-    public void BackPressAccount()
+    public void BackPress()
     {
         AccountBox.LeanMoveLocalX(Screen.width, 0.5f).setEaseInExpo().setOnComplete(AccountPanelExitComplete);
-    }
-    
-    public void BackPressCalendar()
-    {
-        AccountBox.LeanMoveLocalX(-Screen.width, 0.5f).setEaseInExpo().setOnComplete(CalendarPanelExitComplete);
     }
 
     //runs when BackPress() animation is complete
     void AccountPanelExitComplete()
     {
         AccountPanel.SetActive(false);
-    }
-    
-    void CalendarPanelExitComplete()
-    {
-        CalendarPanel.SetActive(false);
     }
     
     //switch ar camera on/off
